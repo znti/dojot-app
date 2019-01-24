cba59af6f9d11ea7d6cfb13757eb430f0563cd7e
znti <znti.rafael@gmail.com>
2019-01-24 08:21:55 -0200

    Add router initial structure with some placeholders - based on https://reacttraining.com/react-router/web/example/basic

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from './Home';
+import About from './About';
+import Topics from './Topics';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload..
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<header className="App-header">
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+							<li>
+								<Link to="/topics">Topics</Link>
+							</li>
+						</ul>
+					</header>
+					<body className="App-body">
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/topics" component={Topics} />
+					</body>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ src/Topic.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Topic = ({ match }) => {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
+}
+export default Topic;
