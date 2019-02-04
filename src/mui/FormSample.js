ab7f1e7d74ee519ae4abe8a9bec255f997606c39
Zana <zanetti.eca@gmail.com>
2019-02-04 17:35:21 +0000

    Form's TextFields are now based on an external array - to be sent through props

--- src/mui/FormSample.js
+++ src/mui/FormSample.js
@@ -11,93 +11,104 @@ const styles = theme => ({
 	root: {
 		width: '100%',
 		marginTop: theme.spacing.unit * 3,
-		overflowX: 'auto',
+	},
+	gridHeader: {
+		paddingTop: theme.spacing.unit * 3,
 	},
 	mainGrid: {
 		padding: theme.spacing.unit * 3,
 	}
 });
 
+const formItems = [{
+		required: true,
+		id: "firstName",
+		name: "firstName",
+		label: "First name",
+		autoComplete: "fname",
+		size: 'M'
+	},{
+		required: true,
+		id: "lastName",
+		name: "lastName",
+		label: "Last name",
+		autoComplete: "lname",
+		size: 'M'
+	},{
+		required: true,
+		id: "address1",
+		name: "address1",
+		label: "Address line 1",
+		fullWidth: true,
+		autoComplete: "billing address-line1",
+		size: 'L'
+	},{
+		id: "address2",
+		name: "address2",
+		label: "Address line 2",
+		fullWidth: true,
+		autoComplete: "billing address-line2",
+		size: 'L'
+	}, {
+		required: true,
+		id: "city",
+		name: "city",
+		label: "City",
+		autoComplete: "billing address-level2",
+		size: 'L'
+	}, {
+		required: true,
+		id: "zip",
+		name: "zip",
+		label: "Zip / Postal code",
+		autoComplete: "billing postal-code",
+		size: 'S'
+	}, {
+		required: true,
+		id: "country",
+		name: "country",
+		label: "Country",
+		autoComplete: "billing country",
+		size: 'S'
+	},{
+		id: "state",
+		name: "state",
+		label: "State/Province/Region" ,
+		size: 'S'
+}];
+
 function FormSample(props) {
 	let { classes } = props;
 	return (
 		<Paper className={classes.root}>
-			<Typography variant="h6" gutterBottom>
+			<Typography className={classes.gridHeader} variant="h6" gutterBottom>
 				Sample details page
 			</Typography>
 			<Grid className={classes.mainGrid} container spacing={24}>
-				<Grid item xs={12} sm={6}>
-					<TextField
-						required
-						id="firstName"
-						name="firstName"
-						label="First name"
-						fullWidth
-						autoComplete="fname"
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TextField
-						required
-						id="lastName"
-						name="lastName"
-						label="Last name"
-						fullWidth
-						autoComplete="lname"
-					/>
-				</Grid>
-				<Grid item xs={12}>
-					<TextField
-						required
-						id="address1"
-						name="address1"
-						label="Address line 1"
-						fullWidth
-						autoComplete="billing address-line1"
-					/>
-				</Grid>
-				<Grid item xs={12}>
-					<TextField
-						id="address2"
-						name="address2"
-						label="Address line 2"
-						fullWidth
-						autoComplete="billing address-line2"
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TextField
-						required
-						id="city"
-						name="city"
-						label="City"
-						fullWidth
-						autoComplete="billing address-level2"
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TextField id="state" name="state" label="State/Province/Region" fullWidth />
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TextField
-						required
-						id="zip"
-						name="zip"
-						label="Zip / Postal code"
-						fullWidth
-						autoComplete="billing postal-code"
-					/>
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TextField
-						required
-						id="country"
-						name="country"
-						label="Country"
-						fullWidth
-						autoComplete="billing country"
-					/>
-				</Grid>
+				{formItems.map(item => {
+					let xs = 12;
+					let sm = 6;
+					switch(item.size.toUpperCase()) {
+						case 'S': // Small
+							sm = 4;
+							break;
+						case 'M': // Medium
+							sm = 6;
+							break;
+						case 'L': // Large
+							sm = 12;
+							break;
+					}
+				
+					return (
+						<Grid item xs={xs} sm={sm}>
+							<TextField
+								fullWidth
+								{...item}
+							/>
+						</Grid>
+					);
+				})}
 				<Grid item xs={12}>
 					<FormControlLabel
 						control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
